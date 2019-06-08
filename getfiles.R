e7a40151645e6145aae058dc78db3a19d3a3199d
Barry Haworth <barryhaworth@gmail.com>
2019-06-08 13:27:12 +1000

    Code to streamline downloading latest version of all reference files (basics, episodes) and to add latest verisons of otheres not currently donloaded (crew, principals)

---
+++ getfiles.R
@@ -0,0 +1,25 @@
+# Get copies of files
+
+library(dplyr)
+library(rmutil)
+
+PROJECT_DIR <- "c:/R/IMDB"
+DATA_DIR    <- "c:/R/IMDB/data"
+FILE_DIR    <- "c:/R/IMDB_Archive/data"
+
+get_title <- function(file){
+  local_file <- paste0(FILE_DIR,"/title.",file,".tsv.gz")
+  print(paste("Local file:",local_file))
+  remote_file <- paste0("https://datasets.imdbws.com/title.",file,".tsv.gz")
+  print(paste("Remote File:",remote_file))
+  if (!file.exists(local_file) |
+      as.Date(file.info(local_file)$mtime) != Sys.Date()){
+    download.file(remote_file,local_file)
+  }
+}
+
+get_title("basics")
+get_title("crew")
+get_title("episode")
+get_title("principals")
+
