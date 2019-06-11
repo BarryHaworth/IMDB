de74444e297347c219ddd24ba028a04a33b8fe05
BarryHaworth <barryhaworth@gmail.com>
2019-06-11 22:26:07 +1000

    Reading Rating History files

--- rating_history.R
+++ rating_history.R
@@ -10,10 +10,6 @@ PROJECT_DIR <- "c:/R/IMDB"
 DATA_DIR    <- "c:/R/IMDB/data"
 FILE_DIR    <- "c:/R/IMDB_Archive/data"
 
-load(file=paste(DATA_DIR,"/ratings.RData",sep=""))
-load(file=paste(DATA_DIR,"/basics.RData",sep=""))
-load(file=paste(DATA_DIR,"/votes.RData",sep=""))
-
 # Function to read a ratings data file and add the date
 read_rat <- function(date){  
   r <- read.delim(paste(FILE_DIR,"/ratings-",date,".tsv.gz",sep=""),stringsAsFactors = FALSE)
@@ -23,28 +19,44 @@ read_rat <- function(date){
 }
 
 # Initialise the Rating History file
-# start.date <- as.Date("2018-05-14")
-# rating.history <- read_rat(start.date)
+end.date   <- Sys.Date()
+start.date <- Sys.Date() - 100
+# start.date <- max(rating.history.full$Date)
+# start.date <- as.Date("2018-05-14")  # The oldest history file I have
+
+if (file.exists(paste0(DATA_DIR,"/rating.history.full.RData"))){
+  load(paste0(DATA_DIR,"/rating.history.full.RData"))
+} else {
+  rating.history.full <- read_rat(start.date)
+}
 
 # Update the Rating History file
-start.date <- Sys.Date() - 10
-# start.date <- max(rating.history$Date)
 
-end.date   <- Sys.Date()
-
-
-# Initialise the Rating History file
-rating.history <- read_rat(start.date)
+days <- as.numeric(end.date - start.date)
+start.time <- Sys.time()
 
 for(d in seq.Date(start.date+1,end.date,1)){
-  # print(file.exists(paste(FILE_DIR,"/ratings-",as.Date(d,origin = "1970-01-01"),".tsv.gz",sep="")))
-  if (file.exists(paste(FILE_DIR,"/ratings-",as.Date(d,origin = "1970-01-01"),".tsv.gz",sep=""))){
-    print(paste("Reading ratings for",as.Date(d,origin = "1970-01-01")))
-    latest <- read_rat(as.Date(d,origin = "1970-01-01")) 
-    rating.history <- rbind(rating.history,latest)
+  d.date <- as.Date(d,origin = "1970-01-01")
+  count <- as.numeric(d.date - start.date)
+  ETA <- Sys.time() + (days-count) * (Sys.time() - start.time)/count
+  if (file.exists(paste0(FILE_DIR,"/ratings-",as.Date(d,origin = "1970-01-01"),".tsv.gz"))){
+    print(paste("Reading ratings for",d.date," Day",count,"of",days,"ETA:",ETA))
+    latest <- read_rat(d.date)
+    rating.history.full <- rbind(rating.history.full,latest) 
   }
 }
 
-rating.history <- rating.history  %>% distinct()
+rating.history.full <- rating.history.full  %>% distinct()
 
-save(rating.history,file=paste(DATA_DIR,"/rating.history.RData",sep=""))
+rating.history.full$tconst <- as.factor(rating.history.full$tconst)
+
+save(rating.history.full,file=paste0(DATA_DIR,"/rating.history.full.RData"))
+
+# Create Subset - latest ratings
+# Most recent 30 days of data
+
+latest.date <- max(rating.history.full$Date)
+
+rating.history.latest <- rating.history.full %>% filter(Date >= latest.date - 30)
+
+save(rating.history.latest,file=paste0(DATA_DIR,"/rating.history.latest.RData"))
